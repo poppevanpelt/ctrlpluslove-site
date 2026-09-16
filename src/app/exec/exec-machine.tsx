feb0d126d8575676e5dc1c21737ff654b0d1fac4
poppevanpelt <82308222+poppevanpelt@users.noreply.github.com>
2026-09-16 10:54:31 +0200

    Isolate EXEC CHUTE machine styles

--- src/app/exec/exec-machine.tsx
+++ src/app/exec/exec-machine.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import styles from "./page.module.css";
+import styles from "./exec-machine.module.css";
 
 const gates = [
   { no: "01", label: "INPUT", action: "REGISTER CHANGE" },
