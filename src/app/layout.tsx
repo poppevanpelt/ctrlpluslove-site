4436a9a3bfefde25ceabead32fbda128e0bf0a94
poppevanpelt <82308222+poppevanpelt@users.noreply.github.com>
2026-09-17 10:02:27 +0200

    Remove global steel ball cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { routeMetadata, SITE_URL } from "./seo";
 import { CtrlLayerProvider } from "./ctrl-layer";
 import { ProjectNavigation } from "./project-navigation";
-import { SteelBallCursor } from "./steel-ball-cursor";
 
 export const metadata: Metadata = {
   ...routeMetadata("/"),
@@ -91,7 +90,6 @@ export default function RootLayout({
         <CtrlLayerProvider>
           <ProjectNavigation />
           {children}
-          <SteelBallCursor />
           <Analytics />
           <SpeedInsights />
         </CtrlLayerProvider>
